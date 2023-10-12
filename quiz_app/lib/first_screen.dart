bc48c3cb873e854322534e14b30687622e5888d5
abdyevas <sabina.abdyeva@ufaz.az>
2023-10-12 22:56:56 +0400

    add button to the first page

--- quiz_app/lib/first_screen.dart
+++ quiz_app/lib/first_screen.dart
@@ -9,15 +9,27 @@ class EnteringScreen extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset('assets/images/quiz-logo.png', width: 200),
-        const SizedBox(height: 20),
+        const SizedBox(height: 70),
         const Text(
           'Learn Flutter the fun way!',
           style: TextStyle(
-            color: Color.fromARGB(255, 217, 179, 255), 
+            color: Color.fromARGB(255, 217, 179, 255),
             fontSize: 24,
-            
           ),
         ),
+        const SizedBox(height: 30),
+        OutlinedButton(
+          onPressed: () {
+            debugPrint('Quiz started');
+          },
+          child: const Text(
+            'Start Quiz',
+            style: TextStyle(
+              color: Color.fromARGB(255, 217, 179, 255),
+              fontSize: 14,
+            ),
+          ),
+        )
       ],
     );
   }
